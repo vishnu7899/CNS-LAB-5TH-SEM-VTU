d391a8f7e1def0a1bcb8d221c0bad05847047147
vishnu7899 <119359369+vishnu7899@users.noreply.github.com>
2023-02-02 17:47:46 +0530

    Update Client.java

--- CNS LAB/PART-B/Client.java
+++ CNS LAB/PART-B/Client.java
@@ -1,56 +1,23 @@
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.util.Scanner;
+import java.net.*;
+import java.io.*;
 public class Client
 {
-public static void main(String args[])throws Exception
+public static void main(String args[]) throws Exception
 {
-String address ="";
-Scanner sc=new Scanner(System.in);
-System.out.println("enter server Address");
-address = sc.nextLine();
-Socket s=new Socket (address,5000);
-DataInputStream din=new DataInputStream(s.getInputStream());
-DataOutputStream dout= new DataOutputStream(s.getOutputStream());
+Socket clientsock=new Socket("10.0.6.31",4000);
+System.out.println("connection successful");
+System.out.println("Enter the file name\n");
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-System.out.println("send get to start");
-String str ="",filename="";
-try
+String fname=br.readLine();
+OutputStream ostream=clientsock.getOutputStream();
+PrintWriter pwrite=new PrintWriter(ostream,true);
+pwrite.println(fname);
+InputStream istream=clientsock.getInputStream();
+BufferedReader socketRead=new BufferedReader(new InputStreamReader(istream));
+String str;
+while((str=socketRead.readLine())!=null)
 {
-while(!str.equals("start"))
-str=br.readLine();
-dout.writeUTF(str);
-dout.flush();
-filename=din.readUTF();
-System.out.println("receving file:"+filename);
-filename="download";
-System.out.println("saving as file:"+filename);
-long sz =Long.parseLong(din.readUTF());
-System.out.println("File Size"+(sz/(1024*1024))+"MB");
-byte b[]=new byte[1024];
-System.out.println("receiving file....");
-FileOutputStream fos =new FileOutputStream(new File(filename),true);
-long bytesRead;
-do
-{
-bytesRead =din.read(b,0,b.length);
-fos.write(b,0,b.length);
-}
-while(!(bytesRead<1024));
-System.out.println("completed");
-fos.close();
-dout.close();
-s.close();
-}
-catch(EOFException e)
-{
+System.out.println(str);
 }
 }
 }
-
