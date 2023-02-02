bbd31935b6c90098e8be65b36bca53317b0fde11
vishnu7899 <119359369+vishnu7899@users.noreply.github.com>
2023-02-02 17:49:59 +0530

    Update UDPServer.java

--- CNS LAB/PART-B/UDPServer.java
+++ CNS LAB/PART-B/UDPServer.java
@@ -1,38 +1,27 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
-
-public class UDPServer {
-	public static byte buf[] = new byte[1024];
-	public static int cport = 789,sport=790;
-	public static void main(String[] args) {
-		
-		
-		try {
-			InetAddress host=InetAddress.getByName("127.0.0.1");
-			DatagramSocket serversocket = new DatagramSocket(5009);
-			DatagramPacket dp = new DatagramPacket(buf,buf.length);
-			Scanner s = new Scanner(System.in);
-			System.out.println("Server is Running...");
-			while(true)
-			{System.out.println("Enter msg");
-				String str = new String(s.next());
-				buf = str.getBytes();
-				
-			//serversocket.receive(dp);
-			//String str = new String(dp.getData(), 0,dp.getLength());
-				if(str.equals("STOP"))
-				{
-				System.out.println("Terminated...");
-				serversocket.send(new DatagramPacket(buf,str.length(),host,5008));
-				break;
-				}
-				DatagramPacket sd = new DatagramPacket(buf,buf.length,host,5008);
-			serversocket.send(sd);
-			}
-		}
-	catch(Exception e) {}
+class UDPS
+{
+public static void main(String args[]) throws Exception 
+{
+DatagramSocket serversocket=new DatagramSocket(9876);
+byte[] sendbuffer = new byte[1024];
+byte[] receivebuffer = new byte[1024];
+while(true)
+{
+DatagramPacket receivepacket = new DatagramPacket(receivebuffer,receivebuffer.length);
+serversocket.receive(receivepacket);
+InetAddress ip= receivepacket.getAddress();
+int portno = receivepacket.getPort();
+String clientdata= new String(receivepacket.getData());
+System.out.println("Client msg "+clientdata);
+System.out.println("Server\n");
+BufferedReader serverread =new BufferedReader(new InputStreamReader(System.in));
+String serverdata = serverread.readLine();
+sendbuffer=serverdata.getBytes();
+DatagramPacket sendpacket = new DatagramPacket(sendbuffer,sendbuffer.length,ip,portno);
+serversocket.send(sendpacket);
 }
-}	
-
+}
+}
 
