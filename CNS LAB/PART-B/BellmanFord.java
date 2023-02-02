b25160fb7a38079cf1d7e2ed32400f3eaa0bde62
vishnu7899 <119359369+vishnu7899@users.noreply.github.com>
2023-02-02 17:47:09 +0530

    Update BellmanFord.java

--- CNS LAB/PART-B/BellmanFord.java
+++ CNS LAB/PART-B/BellmanFord.java
@@ -1,89 +1,84 @@
 import java.util.Scanner;
-
-public class BellmanFord
+public class BellmanFord 
 {
 private int D[];
 private int num_ver;
-public static final int MAX_VALUE = 999;
-
+public static final int MAX_VALUE =999;
 public BellmanFord(int num_ver)
-
 {
-this.num_ver = num_ver;
-D = new int[num_ver + 1];
+this.num_ver=num_ver;
+D=new int[num_ver+1];
 }
-
-public void BellmanFordEvaluation(int source, int A[][])
+public void BellmanFordEvalution(int source,int A[][])
 {
-for (int node = 1; node <= num_ver; node++)
+for(int node=1;node<=num_ver;node++)
 {
-D[node] = MAX_VALUE;
+D[node]=MAX_VALUE;
 }
-D[source] = 0;
-for (int node = 1; node <= num_ver - 1; node++)
+D[source]=0;
+for(int node=1;node<=num_ver-1;node++)
 {
-for (int sn = 1; sn<= num_ver; sn++)
+for(int sn=1;sn<=num_ver;sn++)
 {
-for (int dn = 1; dn<= num_ver; dn++)
+for(int dn=1;dn<=num_ver;dn++)
 {
-if (A[sn][dn] != MAX_VALUE)
+if(A[sn][dn]!=MAX_VALUE)
 {
-if (D[dn] > D[sn]+ A[sn][dn])
-D[dn] = D[sn] + A[sn][dn];
-
+if(D[dn]>D[sn]+A[sn][dn])
+D[dn]=D[sn]+A[sn][dn];
 }
 }
 }
 }
-for (int sn = 1; sn<= num_ver; sn++)
+for(int sn=1;sn<=num_ver;sn++)
 {
-for (int dn = 1; dn<= num_ver; dn++)
+for(int dn=1;dn<=num_ver;dn++)
 {
-if (A[sn][dn] != MAX_VALUE)
+if(A[sn][dn]!=MAX_VALUE)
 {
-if (D[dn] > D[sn]+ A[sn][dn])
-
-System.out.println("The Graph contains negative egde cycle");
+if (D[dn]>D[sn]+A[sn][dn])
+System.out.println("The graph contains negative edge cycle");
 }
 }
 }
-for (int vertex = 1; vertex <= num_ver; vertex++)
+for(int vertex=1;vertex<=num_ver;vertex++)
 {
-System.out.println("distance of source " + source + " to "+ vertex + "is " + D[vertex]);
+System.out.println("Distance of source- "+source+" to "+vertex+" -is- "+D[vertex]);
 }
 }
 
-public static void main(String[ ] args)
+public static void main(String []args)
 {
-int num_ver = 0;
-
+int num_ver=0;
 int source;
-
-Scanner scanner = new Scanner(System.in); System.out.println("Enter the number of vertices"); num_ver = scanner.nextInt();
-
-int A[][] = new int[num_ver + 1][num_ver + 1];
+Scanner scanner =new Scanner(System.in);
+System.out.println("enter the number of vertices");
+num_ver=scanner.nextInt();
+int A[][]= new int[num_ver+1][num_ver+1];
 System.out.println("Enter the adjacency matrix");
-for (int sn = 1; sn<= num_ver; sn++)
+for(int sn=1;sn<=num_ver;sn++)
 {
-for (int dn = 1; dn<= num_ver; dn++)
+for(int dn=1;dn<=num_ver;dn++)
 {
-A[sn][dn] = scanner.nextInt();
-if (sn == dn)
+A[sn][dn]=scanner.nextInt();
+if(sn==dn)
 {
-A[sn][dn] = 0;
+A[sn][dn]=0;
 continue;
 }
-if (A[sn][dn] == 0)
+if(A[sn][dn]==0)
 {
-A[sn][dn] = MAX_VALUE;
+A[sn][dn]=MAX_VALUE;
 }
 }
 }
-
 System.out.println("Enter the source vertex");
-source = scanner.nextInt();
-
-BellmanFord b = new BellmanFord (num_ver); b.BellmanFordEvaluation(source, A); scanner.close();
-
+source=scanner.nextInt();
+BellmanFord b=new BellmanFord(num_ver);
+b.BellmanFordEvalution(source,A);
+scanner.close();
 }
 }
+ 
+
+
